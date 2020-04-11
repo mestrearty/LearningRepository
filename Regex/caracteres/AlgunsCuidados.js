fec4dc3012944913938be9fef0edbc3a5973d009
Pedro Ventura <phventurarodrigues@gmail.com>
2020-04-11 17:59:56 -0300

    Primeiros exemplos de Regex

---
+++ Regex/caracteres/AlgunsCuidados.js
@@ -0,0 +1,9 @@
+const textoUmaLinha = "..."; //aspas simples ou duplas
+
+const textoMultiplasLinhas = `
+Linha 1 
+templatString
+`;
+
+//cuidado com o tab!
+console.log('\tTeste	'.match(/\s/g));
---
+++ Regex/caracteres/OlaRegex.js
@@ -0,0 +1,4 @@
+const texto = 'Casa bonita é casa amarela da esquina com a Rua ACASALAR.';
+
+console.log(texto.match(/casa/gi));
+console.log(texto.match(/a b/));
