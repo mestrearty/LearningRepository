56cc1e5e4ec94fd300901bd555f0e573dad9d0b4
artficer <phventurarodrigues@gmail.com>
2020-04-25 01:22:45 -0300

    Template String

---
+++ TypeScript/ecmascript/templateString.ts
@@ -0,0 +1,7 @@
+const usuarioID: string = "SuporteCod3r";
+const notificacoes: string = "9";
+//usando template string
+const boasVindas = `Boas vindas ${usuarioID}. 
+Notificações: ${notificacoes}`;
+
+console.log(boasVindas)
