57a64344a8bc6a048834157ef1ee6b9fef99a208
PedroUFJF <pedro.ventura@ice.ufjf.br>
2020-05-07 12:00:29 -0300

    Gulp with Babel

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,28 @@
 const gulp = require("gulp");
-const series = gulp.series;
+const { series, parallel } = gulp;
+
+const antes1 = (callback) => {
+  console.log("Tarefa Antes 1!");
+  return callback();
+};
+
+const antes2 = (callback) => {
+  console.log("Tarefa Antes 2!");
+  return callback();
+};
 
 function copiar(callback) {
   console.log("Tarefa de copiar!");
+  gulp
+    //.src(["pastaA/arquivo1.txt", "pastaA/arquivo2.txt"])
+    .src('pastaA/**/*.txt')
+    .pipe(gulp.dest("pastaC"));
   return callback();
 }
 
+const fim = (callback) => {
+  console.log("Tarefa Fim!");
+  return callback();
+};
 
-module.exports.default = series(copiar);
+module.exports.default = series(parallel(antes1, antes2), copiar, fim);
