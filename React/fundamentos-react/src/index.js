bc2315c52a92d28b771ff5affbf0f433c48a4245
ArtFicer <phventurarodrigues@gmail.com>
2021-09-22 14:42:13 -0300

    Criando Diretório e Primeiro Componente

---
+++ React/fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
+import Primeiro from "./components/basicos/Primeiro";
+
+const el = document.getElementById("root");
+
+ReactDOM.render(
+  <div>
+    <Primeiro></Primeiro>
+  </div>,
+  el
+);
