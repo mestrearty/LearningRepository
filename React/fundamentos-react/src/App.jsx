bde72b2347b82283d70b213bfd0bc588a9479453
ArtFicer <phventurarodrigues@gmail.com>
2021-09-22 15:21:39 -0300

    Refatorando para uso de App.jsx

---
+++ React/fundamentos-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
+
+export default (props) => {
+  return (
+    <div id="app">
+      <h1>Fundamentos React</h1>
+      <Primeiro></Primeiro>
+      <ComParametro titulo="Meu Título" subtitulo="Meu Subtítulo" numero={3} />
+      <Fragmento />
+    </div>
+  );
+};
--- React/fundamentos-react/src/index.js
+++ React/fundamentos-react/src/index.js
@@ -1,17 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
-import Primeiro from "./components/basicos/Primeiro";
-import ComParametro from "./components/basicos/ComParametro";
-import Fragmento from "./components/basicos/Fragmento";
+import App from "./App";
 
 const el = document.getElementById("root");
 
-ReactDOM.render(
-  <div>
-    <Primeiro></Primeiro>
-    <ComParametro titulo="Meu Título" subtitulo="Meu Subtítulo" numero={3} />
-    <Fragmento />
-  </div>,
-  el
-);
+ReactDOM.render(<App />, el);
