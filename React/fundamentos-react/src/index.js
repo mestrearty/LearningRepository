76aa17c6a5231cf1a945ce9f6d2c6b720442634d
ArtFicer <phventurarodrigues@gmail.com>
2021-09-22 14:56:18 -0300

    Props

--- React/fundamentos-react/src/index.js
+++ React/fundamentos-react/src/index.js
@@ -2,12 +2,14 @@ import ReactDOM from "react-dom";
 import React from "react";
 import "./index.css";
 import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 const el = document.getElementById("root");
 
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
+    <ComParametro titulo="Meu Título" subtitulo="Meu Subtítulo" numero={3} />
   </div>,
   el
 );
