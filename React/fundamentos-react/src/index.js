ed5912f0799ea7301b1ac4200f1533022c9d2363
ArtFicer <phventurarodrigues@gmail.com>
2021-09-22 15:07:37 -0300

    Fragments

--- React/fundamentos-react/src/index.js
+++ React/fundamentos-react/src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./index.css";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 const el = document.getElementById("root");
 
@@ -10,6 +11,7 @@ ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
     <ComParametro titulo="Meu Título" subtitulo="Meu Subtítulo" numero={3} />
+    <Fragmento />
   </div>,
   el
 );
