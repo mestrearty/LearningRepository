c04814493cc1c545f7a6a2d360ba23f29055aa85
ArtFicer <phventurarodrigues@gmail.com>
2021-09-23 08:50:32 -0300

    Desafio Min e Max para componentes

--- React/fundamentos-react/src/App.jsx
+++ React/fundamentos-react/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import MinMax from "./components/basicos/DesafioMinMax";
 
 export default (props) => {
   return (
@@ -11,6 +12,7 @@ export default (props) => {
       <Primeiro></Primeiro>
       <ComParametro titulo="Meu Título" subtitulo="Meu Subtítulo" numero={3} />
       <Fragmento />
+      <MinMax min={1} max={10} />
     </div>
   );
 };
