970c9b31a499ead43fdc3c028e0f146a40d00560
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-28 22:30:06 -0300

    Primeiro contato com a linguagem

---
+++ Kotlin/kotlin_aula_01/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val nome: String = "Pikachu"
+    val evolucao: String = "Raichu"
+    var dex: Int = 25
+
+    println("O pokémon é o $nome dex: $dex")
+    println("O pokémon é o $evolucao dex: ${++dex}")
+
+}
