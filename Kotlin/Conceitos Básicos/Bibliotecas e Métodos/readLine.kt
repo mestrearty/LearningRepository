1b14af44c1ae0d5ff17bb2965afa9fdd65793d19
Pedro Ventura <phventurarodrigues@gmail.com>
2023-08-03 22:47:23 -0300

    Input de dados no terminal

---
+++ Kotlin/Conceitos Básicos/Bibliotecas e Métodos/readLine.kt
@@ -0,0 +1,15 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    val s = readLine() //realiza a leitura de inputs no terminal
+    
+    if (s != null && s !=""){
+        s.toInt()
+        s.toFloat()
+        s.toBoolean()
+    }else{
+        println("oi")
+    }
+}
